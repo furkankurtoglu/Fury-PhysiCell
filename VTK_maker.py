bf5b85b2e5f132aaf5716205f63db8b61d684398
Furkan Kurtoglu <fkurtog@iu.edu>
2019-12-18 00:17:33 -0500

    Mesh_Data upload

--- VTK_maker.py
+++ VTK_maker.py
@@ -5,68 +5,43 @@ Created on Tue Dec 17 16:27:17 2019
 @author: fkurtog
 """
 
-from pyMCDS import pyMCDS
+
 import scipy.io as sio
-
-mcds = pyMCDS('output00000070.xml')
-me = sio.loadmat('output00000070_microenvironment0.mat')
-me = me['multiscale_microenvironment']
-
-xdim= me[0][:]
-ydim= me[1][:]
-zdim= me[2][:]
-oxygen = me[4][:]
-glucose = me[5][:]
-lactate = me[6][:]
-glutamine = me[7][:]
+import glob
 
 
-#%%
-b= mcds.get_mesh()
-A=mcds.data['mesh']['voxels']['centers']
-
-with open("PhysiCell3D_mesh.csv", 'w') as f:
-#    f.write('# vtk DataFile Version 2.0')
-#    f.write('\n')
-#    f.write('ASCII')
-#    f.write('\n')
-#    f.write('DATASET RECTILINEAR_GRID')
-#    f.write('\n')
-#    f.write('DIMENSIONS 500 800 500')
-#    f.write('\n')
-#    f.write('X_COORDINATES 500 float')
-#    f.write('\n')
-#    for x in xx[0][1]:
-#        f.write(str(x))
-#        f.write(' ')
-#    f.write('\n')
-#    f.write('Y_COORDINATES 800 float')
-#    f.write('\n')
-#    for y in yy[0][1]:
-#        f.write(str(y))
-#        f.write(' ')
-#    f.write('\n')
-#    f.write('Z_COORDINATES 500 float')
-#    f.write('\n')
-#    for z in zz[0][1]:
-#        f.write(str(z))
-#        f.write(' ')
-#    f.write('\n')
-#    f.write('SCALARS scalar_variable float 1')
-    f.write('xdim,ydim,zdim,oxygen,glucose,lactate,glutamine')
-    f.write('\n')
-    for i in range(len(glucose)):
-        f.write(str(xdim[i]))
-        f.write(',')
-        f.write(str(ydim[i]))
-        f.write(',')
-        f.write(str(zdim[i]))
-        f.write(',')
-        f.write(str(oxygen[i]))
-        f.write(',')
-        f.write(str(glucose[i]))
-        f.write(',')
-        f.write(str(lactate[i]))
-        f.write(',')
-        f.write(str(glutamine[i]))
+def me_loader(mat,cnt):
+    me = sio.loadmat(mat)
+    me = me['multiscale_microenvironment']
+    xdim= me[0][:]
+    ydim= me[1][:]
+    zdim= me[2][:]
+    oxygen = me[4][:]
+    glucose = me[5][:]
+    lactate = me[6][:]
+    glutamine = me[7][:]
+    name="PhysiCell3D_mesh_" + str(cnt)+".csv"
+    with open(name, 'w') as f:
+        f.write('xdim,ydim,zdim,oxygen,glucose,lactate,glutamine')
         f.write('\n')
+        for i in range(len(glucose)):
+            f.write(str(xdim[i]))
+            f.write(',')
+            f.write(str(ydim[i]))
+            f.write(',')
+            f.write(str(zdim[i]))
+            f.write(',')
+            f.write(str(oxygen[i]))
+            f.write(',')
+            f.write(str(glucose[i]))
+            f.write(',')
+            f.write(str(lactate[i]))
+            f.write(',')
+            f.write(str(glutamine[i]))
+            f.write('\n')
+            
+output_list = glob.glob("output*0.mat")
+cnt=1
+for mat in output_list:
+    sphere_actor = me_loader(mat,cnt)
+    cnt += 1
